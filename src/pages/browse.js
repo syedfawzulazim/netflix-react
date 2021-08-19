3a06317b6420dc5eb4d9ba60398fc664fa2a2073
Syed Fawzul Azim <adnansyed27@gmail.com>
2021-08-19 21:01:29 +0200

    added

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 export default function Signup() {
     return (
-        <p>Browse</p>
+        <p>Browser </p>
     );
 }
