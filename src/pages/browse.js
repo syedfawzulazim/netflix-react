a10ea3950054eabb94b63f48cc96080f8c19b215
Syed Fawzul Azim <adnansyed27@gmail.com>
2021-08-21 03:05:43 +0200

    use-content hook added

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { useContent } from '../hooks';
+import selectionFilter from '../utils/selection-filter';
 import { BrowserRouter } from 'react-router-dom';
+import { BrowseContainer } from '../containers/browse';
 
 export default function Signup() {
+
+    const { series } = useContent('series');
+    const { films } = useContent('films');
+    const slides = selectionFilter({ series, films });
+
     return (
-        <p>Browser </p>
+
+        <BrowseContainer slides={slides} />
     );
 }
