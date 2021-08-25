9269efb62ddfcbb619393bbaa55dea017d62d9dc
Syed Fawzul Azim <adnansyed27@gmail.com>
2021-08-25 02:16:43 +0200

    card styles added

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,15 +1,18 @@
 import React, { useState, useContext, useEffect } from 'react'
 import { FirebaseContext } from '../context/firebase'
 import { SelectProfileContainer } from './profile';
-import { Header, Loading } from '../components';
+import { Header, Card, Loading } from '../components';
 import * as ROUTES from '../constants/routes';
 import logo from '../logo.svg';
 
 
 export function BrowseContainer({ slides }) {
+
+    const [category, setCategory] = useState('series');
     const [profile, setProfile] = useState({});
     const [loading, setLoading] = useState({});
     const [searchTerm, setSearchTerm] = useState('');
+    const [slideRows, setSlideRows] = useState([]);
 
     const { firebase } = useContext(FirebaseContext)
     const user = firebase.auth().currentUser || {};
@@ -19,6 +22,11 @@ export function BrowseContainer({ slides }) {
             setLoading(false);
         }, 3000);
     }, [profile.displayName])
+
+    useEffect(() => {
+        setSlideRows(slides[category])
+
+    }, [slides, category])
 
     return profile.displayName ? (
         <>
@@ -34,8 +42,16 @@ export function BrowseContainer({ slides }) {
                 <Header.Frame>
                     <Header.Group>
                         <Header.Logo to={ROUTES.HOME} src={logo} alt="NetFlix" />
-                        <Header.TextLink>Series</Header.TextLink>
-                        <Header.TextLink>Films</Header.TextLink>
+                        <Header.TextLink
+                            active={category === 'series' ? 'ture' : 'false'}
+                            onClick={() => setCategory('series')}>
+                            Series
+                        </Header.TextLink>
+                        <Header.TextLink
+                            active={category === 'films' ? 'ture' : 'false'}
+                            onClick={() => setCategory('films')}>
+                            Films
+                        </Header.TextLink>
                     </Header.Group>
 
                     <Header.Group>
@@ -66,6 +82,33 @@ export function BrowseContainer({ slides }) {
                     <Header.PlayButton>Play</Header.PlayButton>
                 </Header.Feature>
             </Header>
+
+            <Card.Group>
+                {slideRows.map((slideItem) => (
+                    <Card key={`${category}-${slideItem.title.toLowerCase()}`} >
+                        <Card.Title>{slideItem.title}</Card.Title>
+                        <Card.Entities>
+                            {slideItem.data.map((item) => (
+                                <Card.Item key={item.dicId} item={item} >
+                                    <Card.Image src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`} />
+                                    <Card.Meta>
+                                        <Card.SubTitle>{item.title}</Card.SubTitle>
+                                        <Card.Text>{item.description}</Card.Text>
+                                    </Card.Meta>
+                                </Card.Item>
+                            ))}
+                        </Card.Entities>
+                        <Card.Feature category={category}>
+                            {/* <Player>
+                                <Player.Button />
+                                <Player.Video src="/videos/bunny.mp4" />
+                            </Player> */}
+                            <p>Hellow</p>
+                        </Card.Feature>
+                    </Card>
+                ))}
+            </Card.Group>
+
         </>
     ) : (
         <SelectProfileContainer user={user} setProfile={setProfile} />
