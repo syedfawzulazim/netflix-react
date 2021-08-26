769dff4134eda67361bfe8429a1879bc2e2f3509
Syed Fawzul Azim <adnansyed27@gmail.com>
2021-08-26 03:31:33 +0200

    card final edits

--- src/components/card/styles/card.js
+++ src/components/card/styles/card.js
@@ -9,6 +9,17 @@ export const Title = styled.p`
     margin-top: 0;
 `;
 
+export const FeatureText = styled.p`
+    font-size: 18px;
+    color: white;
+    font-weight: ${({ fontWeight }) => (fontWeight === 'bold' ? 'bold' : 'normal')};
+    margin: 0;
+
+    @media (max-width: 600px) {
+      line-height: 22px;
+    }
+`;
+
 export const Container = styled.div`
     display:flex;
     flex-direction:column;
@@ -16,7 +27,7 @@ export const Container = styled.div`
     box-sizing: border-box;
 
     > ${Title} {
-        @media (max-width:1000pc) {
+        @media (max-width:1000px) {
             margin-left: 30px;
         }
     }
@@ -34,32 +45,159 @@ export const Group = styled.div`
 
   > ${Container}:first-of-type {
     @media (min-width: 1100px) {
-      margin-top: -100px;
+      margin-top: -150px;
     }
   }
 `;
 
-export const SubTitle = styled.p``;
+export const SubTitle = styled.p`
+  font-size: 12px;
+  color: #fff;
+  font-weight: bold;
+  margin-top: 0;
+  margin-bottom: 0;
+  user-select: none;
+  display: none;
 
-export const Text = styled.p``;
+`;
 
-export const Feature = styled.div``;
+export const Text = styled.p`
+  margin-top: 5px;
+  font-size: 10px;
+  color: #fff;
+  margin-bottom: 0;
+  user-select: none;
+  display: none;
+  line-height: normal;   
+`;
 
-export const FeatureText = styled.p``;
+export const Feature = styled.div`
+  display: flex;
+  flex-direction: row;
+  background: url(${({ src }) => src});
+  background-size: contain;
+  position: relative;
+  height: 360px;
+  background-position-x:right;
+  background-repeat: no-repeat;
+  background-color: black;
 
-export const FeatureTitle = styled(Title)``;
+  @media (max-width: 1000px){
+    height: auto;
+    background-size: auto;
 
-export const FeatureClose = styled.button``;
+    ${Title} {
+      font-size: 20px;
+      line-height: 20px;
+      margin-bottom: 10px;
+    }
+    
+    ${FeatureText}{
+      font-size: 14px;
+    }
+  }
+`;
 
-export const Maturity = styled.div``;
 
-export const Content = styled.div``;
 
-export const Meta = styled.div``;
+export const FeatureTitle = styled(Title)`
+  margin-left:0;
+`;
 
-export const Entities = styled.div``;
+export const FeatureClose = styled.button`
+  color:white;
+  position:absolute;
+  right: 20px;
+  top: 20px;
+  cursor:pointer;
+  background-color: transparent;
+  border: 0;
 
-export const Item = styled.div``;
+  img {
+        filter: brightness(0) invert(1);
+        width:24px;
+    }
+`;
 
-export const Image = styled.img``;
+export const Maturity = styled.div`
+  background-color: ${({ rating }) => (rating >= 15 ? 'red' : 'green')};
+  border-radius: 12px;
+  width:20px;
+  padding: 2px;
+  text-align: center;
+  font-weight: bold;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+  margin-right: 10px;
+  font-size: 12px;
+`;
+
+export const Content = styled.div`
+  margin: 56px;
+  max-width: 500px;
+  line-height: normal;
+
+  @media (max-width: 1000px){
+    margin: 30px;
+    max-width: none;
+  }
+`;
+
+export const Meta = styled.div`
+  display:none;
+  position: absolute;
+  bottom: 0;
+  padding: 10px;
+  background-color: #0000008f; 
+`;
+
+export const Entities = styled.div`
+  display:flex;
+  flex-direction: row;
+`;
+
+export const Item = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-right: 5px;
+  position: relative;
+  cursor: pointer;
+  transition: transform 0.2s;
+
+  &:hover {
+    transform: scale(1.3);
+    z-index: 99;
+  }
+
+  @media (max-width: 1200px){
+    &:hover ${Meta}, &:hover ${Text}, &:hover ${SubTitle} {
+      display: block;
+      z-index: 100;
+    }
+  }
+
+  &:first-of-type {
+    margin-left: 56px;
+
+    @media (max-width: 1000px){
+      margin-left: 30px;
+    }
+  }
+
+  &:last-of-type {
+    margin-right: 56px;
+
+    @media (max-width: 1000px){
+      margin-right: 30px;
+    }
+  }
+`;
+
+export const Image = styled.img`
+  border: 0;
+  width:100%;
+  max-width: 305px;
+  cursor:pointer;
+  padding: 0;
+  margin: 0;
+`;
 
